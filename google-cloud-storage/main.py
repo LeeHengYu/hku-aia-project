2a129f7e9c11a1fde001338d4a36516606817a4e
hengyulee <hengyuli901229@gmail.com>
2026-01-13 19:04:58 +0800

    Added cloud storage utility functions

--- google-cloud-storage/main.py
+++ google-cloud-storage/main.py
@@ -1,6 +1,6 @@
 import os
 from google.cloud import storage
-from typing import Protocol
+from typing import List, Protocol
 
 BUCKET_NAME = "crawled-raw"
 
@@ -28,7 +28,22 @@ def upload_bytes(client, bucket_name, data, destination_blob_name, blob_prefix=N
     blob.upload_from_string(data)
     print(f"Uploaded bytes to gs://{bucket_name}/{destination_blob_name}")
 
+def list_blobs(client, bucket_name, prefix=None) -> List[storage.Blob]:
+    bucket = client.bucket(bucket_name)
+    blobs = bucket.list_blobs(prefix=prefix)
+    return list(blobs)
+
+def download_blob(client, bucket_name, source_blob_name, destination_file_name):
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
 if __name__=="__main__":
     cli = storage.Client()
-    # upload_folder(cli, BUCKET_NAME, 'google-cloud-storage/test_folder', 'data')
+    
+    # SAMPLE USE CASES
+    upload_folder(cli, BUCKET_NAME, 'google-cloud-storage/test_folder', 'data')
     upload_bytes(cli, BUCKET_NAME, b"hello world", "test_bytes.txt")
+    
+    aia_files = list_blobs(cli, 'crawled-clean', 'aia')
+    download_blob(cli, 'crawled-clean', aia_files[0].name, 'test_download.pdf')
